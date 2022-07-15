b355047b7783cea0f26c6ff233ef1a8bc2fc69bd
John McPherson <john.mcpherson@xtra.co.nz>
2022-07-16 10:41:17 +1200

    Add UpdateEmailActivity

--- app/src/main/java/com/example/firebaseauthentication/HomeActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/HomeActivity.java
@@ -2,13 +2,22 @@ package com.example.firebaseauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.firebaseauthentication.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.updateEmailBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, UpdateEmailActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/firebaseauthentication/UpdateEmailActivity.java
@@ -0,0 +1,20 @@
+package com.example.firebaseauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firebaseauthentication.databinding.ActivityUpdateEmailBinding;
+
+public class UpdateEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityUpdateEmailBinding binding = ActivityUpdateEmailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.backArrow.setOnClickListener(view -> {
+            finish();
+        });
+    }
+}
