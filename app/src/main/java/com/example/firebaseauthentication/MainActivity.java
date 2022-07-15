638271af32d0030dbde6c79967a90598b106f12a
John McPherson <john.mcpherson@xtra.co.nz>
2022-07-15 21:28:38 +1200

    MainActivity, SignUpActivity, HomeActivity built and partly wired up

--- app/src/main/java/com/example/firebaseauthentication/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.firebaseauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView    create_account_txt;
+    Button sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        create_account_txt = findViewById(R.id.create_account_txt);
+        sign_in = findViewById(R.id.sign_in_button);
+
+        create_account_txt.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+
+        });
+
+        sign_in.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ImageView back_arrow;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        back_arrow = findViewById(R.id.back_arrow);
+
+        back_arrow.setOnClickListener(view -> {
+            finish();
+        });
     }
 }
