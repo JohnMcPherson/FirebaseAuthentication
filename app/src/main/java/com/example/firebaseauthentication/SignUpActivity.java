b18dc6645bbfaffccec2889453ef1c455d0249f7
John <john.mcpherson@xtra.co.nz>
2022-07-30 21:35:21 +1200

    Validation working for Sign up screen

--- app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.firebaseauthentication;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,6 +13,8 @@ import com.example.firebaseauthentication.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    ValidateInput validateInput;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,25 @@ public class SignUpActivity extends AppCompatActivity {
         ActivitySignUpBinding binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        validateInput = new ValidateInput(
+                this,
+                binding.signUpEmail,
+                binding.signUpPassword,
+                binding.repeatPassword);
+
         binding.backArrow3.setOnClickListener(view -> {
             finish();
+        });
+
+        binding.signUpButton.setOnClickListener(view -> {
+            boolean emailVerified = validateInput.validateEmail();
+            boolean passwordVerified = validateInput.validatePassword();
+            boolean repeatPasswordVerified = validateInput.repeatPasswordValidation();
+
+            if (emailVerified && passwordVerified && repeatPasswordVerified) {
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/firebaseauthentication/ValidateInput.java
+++ app/src/main/java/com/example/firebaseauthentication/ValidateInput.java
@@ -7,7 +7,9 @@ import android.widget.Toast;
 
 class ValidateInput {
     private Context context;
-    private EditText email, password;
+    private EditText email, password, repeatPassword;
+
+    String emailInput, passwordInput, repeatPasswordInput;
 
     ValidateInput(Context myContext, EditText myEmail, EditText myPassword) {
         context = myContext;
@@ -15,8 +17,15 @@ class ValidateInput {
         password = myPassword;
     }
 
+    ValidateInput(Context myContext, EditText myEmail, EditText myPassword, EditText myRepeatPassword) {
+        context = myContext;
+        email = myEmail;
+        password = myPassword;
+        repeatPassword = myRepeatPassword;
+    }
+
     boolean validateEmail() {
-        String emailInput = email.getText().toString().trim();
+        emailInput = email.getText().toString().trim();
         if (emailInput.isEmpty()) {
             Toast.makeText(context, "Please enter your email address", Toast.LENGTH_SHORT).show();
             return false;
@@ -29,12 +38,27 @@ class ValidateInput {
     }
 
     boolean validatePassword() {
-        String passwordInput = password.getText().toString().trim();
+        passwordInput = password.getText().toString().trim();
         if (passwordInput.isEmpty()) {
             Toast.makeText(context, "Please enter your password", Toast.LENGTH_SHORT).show();
             return false;
         } else if (passwordInput.length() < 8) {
             Toast.makeText(context, "Please enter a valid password", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    boolean repeatPasswordValidation() {
+        passwordInput = password.getText().toString().trim();
+        repeatPasswordInput = repeatPassword.getText().toString().trim();
+
+        if (repeatPasswordInput.isEmpty()) {
+            Toast.makeText(context, "Fill out all fields", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!repeatPasswordInput.equals(passwordInput)) {
+            Toast.makeText(context, "Passwords don't match", Toast.LENGTH_SHORT).show();
             return false;
         } else {
             return true;
