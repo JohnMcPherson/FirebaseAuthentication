0b99eff4ab15dbd77dda71e266886525d1c0b370
John <john.mcpherson@xtra.co.nz>
2022-07-31 14:39:39 +1200

    Able to sign up

--- app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/SignUpActivity.java
@@ -1,26 +1,43 @@
 package com.example.firebaseauthentication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.firebaseauthentication.databinding.ActivityMainBinding;
 import com.example.firebaseauthentication.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
     ValidateInput validateInput;
+    private FirebaseAuth mAuth;
+
+    String email, password;
+    ActivitySignUpBinding binding;
+    AlertDialog.Builder builder;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivitySignUpBinding binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        builder = new AlertDialog.Builder(this);
 
         validateInput = new ValidateInput(
                 this,
@@ -33,14 +50,42 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
         binding.signUpButton.setOnClickListener(view -> {
-            boolean emailVerified = validateInput.validateEmail();
-            boolean passwordVerified = validateInput.validatePassword();
-            boolean repeatPasswordVerified = validateInput.repeatPasswordValidation();
-
-            if (emailVerified && passwordVerified && repeatPasswordVerified) {
-                Intent intent = new Intent(this, HomeActivity.class);
-                startActivity(intent);
-            }
+            signUpNewAccount();
         });
+
+    }
+
+    public void signUpNewAccount() {
+        loadingAnimation();
+
+        boolean emailVerified = validateInput.validateEmail();
+        boolean passwordVerified = validateInput.validatePassword();
+        boolean repeatPasswordVerified = validateInput.repeatPasswordValidation();
+
+        if (emailVerified && passwordVerified && repeatPasswordVerified) {
+            email = binding.signUpEmail.getText().toString().trim();
+            password = binding.signUpPassword.getText().toString().trim();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Fatal error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    public void loadingAnimation() {
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.loading, null));
+        builder.setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
+
+
