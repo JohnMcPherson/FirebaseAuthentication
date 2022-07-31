09b463d1a237186b82c4e4810038594572be50a2
John <john.mcpherson@xtra.co.nz>
2022-07-31 21:19:08 +1200

    Display login details on Home Activity

--- app/src/main/java/com/example/firebaseauthentication/HomeActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/HomeActivity.java
@@ -6,14 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.firebaseauthentication.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        if(mUser != null) {
+            binding.emailAddressTxt.setText(mUser.getEmail());
+            binding.idTxt.setText(mUser.getUid());
+        }
 
         binding.updateEmailBtn.setOnClickListener(view -> {
             Intent intent = new Intent(this, UpdateEmailActivity.class);
